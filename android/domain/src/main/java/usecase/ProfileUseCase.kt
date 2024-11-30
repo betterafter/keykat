ecc2dbbfc033f4629d1894be0484865c8aca3bbb
keykat <koin12000@naver.com>
2024-12-01 05:01:44 +0900

    [feature] usecase 추가

---
+++ android/domain/src/main/java/usecase/ProfileUseCase.kt
@@ -0,0 +1,29 @@
+package usecase
+
+import com.keykat.domain.profile.entity.EducationEntity
+import com.keykat.domain.profile.entity.ProfileEntity
+import com.keykat.domain.profile.entity.TechEntity
+import com.keykat.domain.profile.repository.EducationRepository
+import com.keykat.domain.profile.repository.ProfileRepository
+import com.keykat.domain.profile.repository.TechStackRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+
+class ProfileUseCase(
+    private val profileRepository: ProfileRepository,
+    private val educationRepository: EducationRepository,
+    private val techStackRepository: TechStackRepository
+) {
+    suspend fun getTopProfile(): Flow<ProfileEntity> {
+        return profileRepository.getProfile()
+    }
+
+    suspend fun getBottomProfile(): Flow<Pair<List<EducationEntity>, List<TechEntity>>> {
+        return educationRepository.getEducationList().combine(
+            flow = techStackRepository.getTechStack(),
+            transform = { f1, f2 ->
+                Pair(f1, f2)
+            }
+        )
+    }
+}
