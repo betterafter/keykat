3f812d1da3348ca3da7448d9eee029fbb768ade9
keykat <koin12000@naver.com>
2024-12-03 00:30:22 +0900

    [feature] tech api 개발

--- react/app/api/profile/education/route.ts
+++ react/app/api/profile/education/route.ts
@@ -3,19 +3,23 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     try {
         const data = `{
-        "name": "keykat",
-        "tel": "010-6850-1740",
-        "email": "koin20000@gmail.com",
-        "profile_url": "https://avatars.githubusercontent.com/u/26290540?v=4",
-        "introduce": "안녕하세요.",
-        "sns": [
-            {
-                "name": "instagram",
-                "url": "https://www.instagram.com/keykat77/",
-                "icon": ""
-            }
-        ]
-    }`;
+            "education": [
+                {
+                "name": "네이버 부스트캠프 웹-모바일 6기",
+                "where": "네이버 커넥트재단",
+                "start_at": "2021.07.14",
+                "end_at": "2021.12.20",
+                "content": "어떤 공부를 했나요?"
+                },
+                {
+                "name": "건국대학교",
+                "where": "건국대학교",
+                "start_at": "2016.03.02",
+                "end_at": "2023.02.20",
+                "content": "어떤 공부를 했나요?"
+                }
+            ]
+        }`;
 
         return NextResponse.json(data);
     } catch (error) {
