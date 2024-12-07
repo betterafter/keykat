2619b7fa759d37b72ed7626f41aa234f2e6301c3
keykat <koin12000@naver.com>
2024-12-08 08:27:43 +0900

    [feature] 파일 추가

---
+++ android/presentation/src/main/java/com/keykat/presentation/screen/career/CareerDetailSectionWidget.kt
@@ -0,0 +1,22 @@
+package com.keykat.presentation.screen.career
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CareerDetailSectionWidget() {
+    Box(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+            .background(Color.Blue)
+    ) {
+
+    }
+}
---
+++ android/presentation/src/main/java/com/keykat/presentation/screen/career/CareerSummarySectionWidget.kt
@@ -0,0 +1,15 @@
+package com.keykat.presentation.screen.career
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CareerSummarySectionWidget() {
+
+}
