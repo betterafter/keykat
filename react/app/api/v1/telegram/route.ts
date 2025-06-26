0a0aff10c05b8a30847f71999d65122d6a91dba4
keykat <koin20000@gmail.com>
2025-06-26 11:09:10 +0900

    feat: 테스트용 webhook 추가

---
+++ react/app/api/v1/telegram/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  if (body?.chat_member) {
+    const { from, chat, old_chat_member, new_chat_member } = body.chat_member;
+
+    const isJoinEvent =
+      old_chat_member.status === "left" &&
+      ["member", "administrator", "creator"].includes(new_chat_member.status);
+
+    if (isJoinEvent) {
+      const userId = from.id;
+      const channelId = chat.id;
+      const username = from.username;
+
+      console.log(`✅ User @${username} (${userId}) joined ${channelId}`);
+
+      // TODO: 포인트 지급 로직 실행
+    }
+  }
+
+  return NextResponse.json({ message: "ok" });
+}
